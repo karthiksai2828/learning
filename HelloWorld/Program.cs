ecd910eff808b8279b8b5d65ac4ec2e7c1b94604
Karthik Manchikatla <kamanchi@microsoft.com>
2018-06-25 12:54:21 +0530

    Initial Commit comment

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n Recursion : Find the factorial of a given number :");
+            Console.WriteLine("-------------------------------------------------------");
+
+            Console.Write(" Input any positive number : ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            long fact = FactorialCalcu(n1);
+            Console.WriteLine(" The factorial of {0} is : {1} ", n1, fact);
+            Console.ReadKey();
+        }
+
+        private static long FactorialCalcu(int n1)
+        {
+            if (n1 == 0)
+            {
+                return 1;
+            }
+            return n1 * FactorialCalcu(n1 - 1);
+        }
+    }
+}
